4abb887e3a3d8edba1e12183b7ca6993c0267a4e
John Buragay <mccano@protonmail.com>
2021-08-20 13:01:37 +0600

    Sports List Menu Bar

--- resources/js/components/SportsList.vue
+++ resources/js/components/SportsList.vue
@@ -1,17 +1,63 @@
 <template>
     <div class="container">
-
+        <div class="row text-center p-3">
+            <div class="col sports-list-menu">
+                <a href="sports/league">
+                    <span><img src="/image/icons/league.svg" alt="F"></span>
+                    <span class="d-none d-lg-inline">Leagues</span>
+                </a>
+            </div>
+            <div class="col sports-list-menu">
+                <a href="sports/nfl">
+                    <span><img src="/image/icons/nfl.svg" alt="F"></span>
+                    <span class="d-none d-lg-inline">NFL</span>
+                </a>
+            </div>
+            <div class="col sports-list-menu">
+                <a href="sports/basketball">
+                    <span><img src="/image/icons/basketball.svg" alt="F"></span>
+                    <span class="d-none d-lg-inline">BasketBall</span>
+                </a>
+            </div>
+            <div class="col sports-list-menu">
+                <a href="sports/baseball">
+                    <span><img src="/image/icons/baseball.svg" alt="F"></span>
+                    <span class="d-none d-lg-inline">Baseball</span>
+                </a>
+            </div>
+            <div class="col sports-list-menu">
+                <a href="sports/hockey">
+                    <span><img src="/image/icons/hockey.svg" alt="F"></span>
+                    <span class="d-none d-lg-inline">Hockey</span>
+                </a>
+            </div>
+            <div class="col sports-list-menu">
+                <a href="sports/fight">
+                    <span><img src="/image/icons/fight.svg" alt="F"></span>
+                    <span class="d-none d-lg-inline">Fighting</span>
+                </a>
+            </div>
+            <div class="col sports-list-menu">
+                <a href="sports/soccer">
+                    <span><img src="/image/icons/soccer.svg" alt="F"></span>
+                    <span class="d-none d-lg-inline">Soccer</span>
+                </a>
+            </div>
+            <div class="col sports-list-menu">
+                <a href="sports">
+                    <span class="d-none d-lg-inline">All Sports</span>
+                    <span><img src="/image/icons/more.svg" alt="F"></span>
+                </a>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-
-
 export default {
     name: "SportsList",
 }
 </script>
 
 <style scoped>
-
 </style>
